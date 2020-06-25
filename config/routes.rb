eb53554db4b80b31897dd03e8ef5f766075d49bf
Aran Roig <aranseraroig@gmail.com>
2020-06-25 17:27:02 +0200

    Wat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
   Rails.application.routes.draw do
-    # resources :users # NOOOOOOOOOOOOO
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    post 'api/user/register', to: 'account#register'
-    post 'api/user/login', to: 'account#login'
+
+    root to: 'home#index'
+    # Api routes
+    post 'api/user/register', to: 'api#register'
+    post 'api/user/login', to: 'api#login'
   end
